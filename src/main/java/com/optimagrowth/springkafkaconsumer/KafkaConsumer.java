6134b143558f6bea48fea62a121041414f8bd059
Henry Osei <rocahenry@gmail.com>
2024-05-26 21:50:03 +0000

    spring boot kafka consumer

---
+++ src/main/java/com/optimagrowth/springkafkaconsumer/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.optimagrowth.springkafkaconsumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+
+    @KafkaListener(topics = "${kafka.topic}")
+    public void listen(Message<String> topic) {
+        System.out.println(topic.getPayload()  );;
+    }
+}
