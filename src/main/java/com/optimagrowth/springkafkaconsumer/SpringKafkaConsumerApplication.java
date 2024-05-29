918965723f70f25dd80ef5890faa7270b9e20e2f
Henry Osei <rocahenry@gmail.com>
2024-05-29 23:57:18 +0000

    spring boot kafka

--- src/main/java/com/optimagrowth/springkafkaconsumer/SpringKafkaConsumerApplication.java
+++ src/main/java/com/optimagrowth/springkafkaconsumer/SpringKafkaConsumerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringKafkaConsumerApplication {
 
+    //consume
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaConsumerApplication.class, args);
     }
